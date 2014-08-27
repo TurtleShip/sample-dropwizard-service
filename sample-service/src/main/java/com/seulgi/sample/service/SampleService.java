ab481d2df8ded327f4d35e14019f758ed4384fcb
Seulgi Kim <sk765@cornell.edu>
2014-08-26 23:22:40 -0700

    Added frontend to my sample serivce.

--- sample-service/src/main/java/com/seulgi/sample/service/SampleService.java
+++ sample-service/src/main/java/com/seulgi/sample/service/SampleService.java
@@ -4,10 +4,17 @@ import com.seulgi.sample.service.config.SampleConfiguration;
 import com.seulgi.sample.service.health.HelloHealthcheck;
 import com.seulgi.sample.service.resources.HelloResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class SampleService extends Application<SampleConfiguration> {
+
+    private static final String FRONTEND_URL_PREFIX = "/";
+    private static final String FRONTEND_REPO = "/assets";
+    private static final String INDEX_FILE = "index.html";
+
+    private static final String BACKEND_URL_PATTERN = "/api/*";
 
 
     public static void main(String[] args) throws Exception {
@@ -16,13 +23,14 @@ public class SampleService extends Application<SampleConfiguration> {
 
     @Override
     public void initialize(Bootstrap<SampleConfiguration> bootstrap) {
-
+        bootstrap.addBundle(new AssetsBundle(FRONTEND_REPO, FRONTEND_URL_PREFIX, INDEX_FILE));
     }
 
     @Override
     public void run(SampleConfiguration configuration, Environment environment) throws Exception {
-        environment.jersey().setUrlPattern("/api/*");
+        environment.jersey().setUrlPattern(BACKEND_URL_PATTERN);
         environment.jersey().register(new HelloResource());
         environment.healthChecks().register("HelloHealthCheck", new HelloHealthcheck());
     }
+    
 }
